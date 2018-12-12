625ce1b41c4808fd929a453657894309861eb112
Vishal Malu <vishal.malu@gmail.com>
2018-12-12 12:50:13 +0530

    Add files via upload

---
+++ precache-manifest.b485cb61af5582b1c6c178390f16b427.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ebb1f3b07c2d71d74495",
+    "url": "/static/css/main.4d5100e8.chunk.css"
+  },
+  {
+    "revision": "ebb1f3b07c2d71d74495",
+    "url": "/static/js/main.ebb1f3b0.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c19dd6dc8539c382252f3399c9c6d81a",
+    "url": "/index.html"
+  }
+];
